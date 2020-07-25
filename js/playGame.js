49b9960aa259405c6d16eca0425d22b75a594e5d
Marcelo da Silveira Siedler <siedler@gmail.com>
2020-07-25 00:00:51 -0300

    Básico - Aula 06

--- js/playGame.js
+++ js/playGame.js
@@ -7,11 +7,22 @@ class PlayGame extends Phaser.Scene
         this.passos; //de quanto em quanto eu quero andar
         this.premios;
         this.inimigos;
+
+        this.txtPontos;
+        this.txtVidas;
+
+        this.pontos;
+        this.vidas;
+
+
+        this.estrela;
+
+        
     }
 
     preload()
     {
-        console.log("PRELOAD");
+       
         this.load.image("star", "assets/star.png");
         this.load.image("rock", "assets/rocha.png");
         this.load.spritesheet("personagem", "assets/dude.png", {frameWidth:32, frameHeight:48})
@@ -21,27 +32,54 @@ class PlayGame extends Phaser.Scene
     {
         var chao = this.add.rectangle(0,500,800,30,0xFF8C00).setOrigin(0,0);
         this.physics.add.existing(chao);
+
+        this.physics.world.on("worldbounds", this.saiudaCena);
+
         chao.body.allowGravity = false;
         chao.body.setImmovable(true);
 
-
-        this.premios = this.physics.add.group({
+         this.premios = this.physics.add.group({
             key:'star',
+            repeat:11,
+            collideWorldBounds:true,
+            setXY:{x:20, y:0, stepX:60} 
             
         });
-        
-        var rocha = this.physics.add.image(230,100,'rock');
+
+        this.premios.children.iterate(this.configuracaoFilho);
+       
+        this.inimigos = this.physics.add.group({
+            key:'rock',
+            repeat:4,
+            collideWorldBounds:true,
+            setXY:{x:50, y:-100, stepX:130}
+            
+        });
+
+        this.inimigos.children.iterate(this.configuracaoFilho);
+
         this.personagem = this.physics.add.image(320,470,'personagem',4);
 
         this.input.keyboard.on("keydown_LEFT", ()=> this.teclado('ESQUERDA'));
         this.input.keyboard.on("keydown_RIGHT", ()=> this.teclado('DIREITA'));
         this.input.keyboard.on("keydown_DOWN", ()=> this.teclado('BAIXO'));
+        this.input.keyboard.on("keydown_SPACE", ()=> this.teclado('ESPACO'));
 
         this.physics.add.collider(this.personagem, chao);
-        this.physics.add.collider(this.personagem, this.premios, this.pegou, null, this);
-        this.physics.add.collider(this.personagem, rocha, this.pegou, null, this);
+        this.physics.add.overlap(this.personagem, this.premios, this.pegou, null, this);
+        this.physics.add.overlap(this.personagem, this.inimigos, this.pegou, null, this);
         
-        this.passos = 70;
+        this.passos = 100;
+
+        //Pontos e Vidas 
+        this.add.text(30,chao.y+60, "Pontos:", {fontSize:'16px', fill:'red'});
+        this.add.text(630, chao.y+60, "Vidas:", {fontSize:'16px', fill:'blue'})
+
+        this.pontos = 0;
+        this.txtPontos = this.add.text(100,chao.y+60, this.pontos, {fontSize:'16px', fill:'blue'});
+
+        this.vidas = 2;
+        this.txtVidas = this.add.text(690, chao.y+60, this.vidas, {fontSize:'16px', fill:'red'})
     }
 
     teclado(tecla)
@@ -56,6 +94,11 @@ class PlayGame extends Phaser.Scene
             case 'BAIXO':
                 this.personagem.setVelocityX(0);
                 break;
+            case 'ESPACO':
+                if(this.pontos==10 || this.vidas==0){
+                this.scene.restart();
+                }
+                break;
         
             default:
                 break;
@@ -69,12 +112,29 @@ class PlayGame extends Phaser.Scene
 
     pegou(personagem, item)
     {
+        item.disableBody(true,true);
         switch (item.texture.key) {
             case "star":
-                console.log("Pegou a estrela");
+                this.pontos++;
+                this.txtPontos.text = this.pontos;
+                if(this.pontos==10)
+                {
+                    this.add.text(200,150, "Parabéns você venceu\nTecle ESPAÇO para jogar novamente",
+                    {fontSize:'22px', fill:'orange', backgroundColor:'black'});
+                    personagem.disableBody(true,true);
+
+                }
                 break;
             case "rock":
-                console.log("Pegou a rocha");
+                this.vidas--;
+                this.txtVidas.text = this.vidas;
+                if(this.vidas==0)
+                {
+                    this.add.text(200,150, "Você Perdeu\nTecle ESPAÇO para tentar novamente",
+                    {fontSize:'22px', fill:'orange', backgroundColor:'black'});
+                    personagem.disableBody(true,true);
+
+                }
                 break;
             
             default:
@@ -83,5 +143,17 @@ class PlayGame extends Phaser.Scene
         
     }
 
+    configuracaoFilho(elemento){
+        elemento.body.onWorldBounds = true;
+        elemento.x = Phaser.Math.Between(0,800);
+        elemento.y = Phaser.Math.Between(0,70);
+    }
 
+    saiudaCena(elemento){
+        elemento.x = Phaser.Math.Between(0,800);
+        elemento.y = Phaser.Math.Between(0,70);
+        
+    }
+
+   
 }
